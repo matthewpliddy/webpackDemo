fef255f8dbff4ea321abc37323fe3ca7759ec990
Matt Liddy <matthewpliddy@gmail.com>
2020-05-01 18:10:35 -0400

    Completed Tutorial 5

--- webpack.config.js
+++ webpack.config.js
@@ -3,24 +3,31 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: {
     app: './src/index.js',
-    print: './src/print.js',
   },
   devtool: 'inline-source-map',
-     devServer: {
-         contentBase: './dist',
-      },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
   plugins: [
-    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'Hot Module Replacement',
     }),
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
   },
 };
